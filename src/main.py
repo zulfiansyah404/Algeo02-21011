115eddec884ca91567d665234fe02ae74e6c0264
M Zulfiansyah Bayu Pratama <16521512@std.stei.itb.ac.id>
2022-11-22 17:08:39 +0700

    Membuat rapi GUI

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 import tkinter
-from tkinter import filedialog
+import numpy as np
 import tkinter.messagebox
 import customtkinter
+from PIL import Image, ImageTk
+import os
+from tkinter import filedialog
+import time
+from tess import answer
+import cv2
+
+start = time.time()
+
 customtkinter.set_appearance_mode("System") # Modes: "System" (standard), "Dark",
 "Light"
 customtkinter.set_default_color_theme("blue") # Themes: "blue" (standard), "green",
@@ -10,29 +19,35 @@ customtkinter.set_default_color_theme("blue") # Themes: "blue" (standard), "gree
 global dataset
 dataset = ''
 global filename
-filename = 'Gambar'
-
+global hasil
+global solve
+filename = ''
+hasil = np.array([[]])
+solve = False
+global execute_time
+execute_time = -1
 class App(customtkinter.CTk):
-    WIDTH = 780
+    WIDTH = 1000
     HEIGHT = 520
 
     def __init__(self):
         
         super().__init__()
-        print(filename)
         self.title("Tubes Algeo")
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         self.protocol("WM_DELETE_WINDOW", self.on_closing) # call .on_closing() when app gets closed
-        # ============ create two frames ============
-        # configure grid layout (2x1)
+        self.minsize(App.WIDTH, App.HEIGHT)
+        self.maxsize(App.WIDTH, App.HEIGHT)
+        self.resizable(False, False)
+
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
-        self.frame_left = customtkinter.CTkFrame(master=self,
+        self.frame_left = customtkinter.CTkFrame(master=self,border_width=1,
         width=180,
         corner_radius=0)
         self.frame_left.grid(row=0, column=0, sticky="nswe")
-        self.frame_right = customtkinter.CTkFrame(master=self)
-        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+        self.frame_right = customtkinter.CTkFrame(master=self ,width=1000, corner_radius=0)
+        self.frame_right.grid(row=0, column = 1, sticky="nswe", padx=10, pady=10)
         # ============ frame_left ============
         # configure grid layout (1x11)
         self.frame_left.grid_rowconfigure(0, minsize=10) # empty row with minsize as spacing
@@ -40,73 +55,122 @@ class App(customtkinter.CTk):
         self.frame_left.grid_rowconfigure(8, minsize=20) # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(11, minsize=10) # empty row with minsize as spacing
         self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
-        text="Tubes Algeo 2",
+        text="Face Recognition",
         text_font=("Roboto Medium", -16)) # font name and size in px
         self.label_1.grid(row=1, column=0, pady=10, padx=10)
         self.button_1 = customtkinter.CTkButton(master=self.frame_left,
-        text="Upload",
-        command=self.UploadAction)
+        text="Insert Your Dataset",
+        command=self.UploadActionfolder)
         self.button_1.grid(row=2, column=0, pady=10, padx=20)
         self.button_2 = customtkinter.CTkButton(master=self.frame_left,
-        text="Aksi",
-        command=self.button_event)
-        self.button_2.grid(row=3, column=0, pady=10, padx=20)
+        text="Insert Your Image", compound="folder",
+        command=self.UploadActionImage)
+        self.button_2.grid(row=3, column=0, pady=10, padx=10)
+
+        self.button_3 = customtkinter.CTkButton(master=self.frame_left,
+        text="Process", compound="folder",
+        command=self.process)
+        self.button_3.grid(row=4, column=0, pady=10, padx=10)
+
         self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="AppearanceMode:")
         self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
         self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_left,
         values=["Light", "Dark"],
         command=self.change_appearance_mode)
         self.optionmenu_1.grid(row=10, column=0, pady=10, padx=20, sticky="w")
-        # ============ frame_right ============
-        # configure grid layout (3x7)
-        self.frame_right.rowconfigure((1, 1, 1, 1), weight=1)
-        self.frame_right.rowconfigure(0, weight=1)
-        self.frame_right.columnconfigure((1, 1), weight=1)
-        self.frame_right.columnconfigure(2, weight=0)
-        self.frame_info = customtkinter.CTkFrame(master=self.frame_right)
-        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=2, pady=20, padx=20,
-        sticky="nsew")
-        # ============ frame_info ============
-        # configure grid layout (1x1)
-        self.frame_info.rowconfigure(0, weight=1)
-        self.frame_info.columnconfigure(0, weight=1)
-        self.label_info_1 = customtkinter.CTkLabel(master=self.frame_info,
-        text="Gambar" ,
-        corner_radius=6, # <- custom corner radius
-        fg_color=("white", "gray38"), # <- custom tuple-color
-        justify=tkinter.LEFT)
-        self.label_info_1.grid(column=0, row=0, sticky="nwe")
-        
-        self.frame_info2 = customtkinter.CTkFrame(master=self.frame_right)
-        self.frame_info2.grid(row=1, column=0, columnspan=2, rowspan=2, pady=20, padx=20,
-        sticky="nsew")
-        # ============ frame_info ============
-        # configure grid layout (1x1)
-        self.frame_info2.rowconfigure(0, weight=1)
-        self.frame_info2.columnconfigure(0, weight=1)
-
-        self.label_info_2 = customtkinter.CTkLabel(master=self.frame_info2,
-        text="Gambar 2" ,
-        fg_color=("white", "gray38"), # <- custom tuple-color
-        justify=tkinter.RIGHT)
-        self.label_info_2.grid(column=0, row=0, sticky="nwe")
-
-        # ============ frame_right ============
   
+        # ============ frame_right ============
+        # configure grid layout (1x1)
+        self.frame_right.grid_rowconfigure(0, weight=1)
+        self.frame_right.grid_columnconfigure(0, weight=1)
+        self.frame_picture = customtkinter.CTkFrame(master=self.frame_right, width=320, border_width=10) # font name and size in px
+        self.frame_picture.grid(row=1, column=0, sticky="nswe")
+
+        self.frame_dataset = customtkinter.CTkFrame(master=self.frame_right, width=320, border_width=10) # font name and size in px
+        self.frame_dataset.grid(row=1, column=1, sticky="nswe")
+
         
-        # self.check_box_1.configure(state=tkinter.DISABLED, text="CheckBox disabled")
-        # self.check_box_2.select()
-    def UploadAction(self):
+        self.label_2 = customtkinter.CTkLabel(master=self,
+        text=str(execute_time),
+        text_font=("Roboto Medium", -10))
+        self.label_2.grid(row=0, column=2, sticky="nswe")
+
+
+        # Buat label di frame picture
+        self.label_image = customtkinter.CTkLabel(master=self.frame_picture,
+        text="Image", fg_color="#0c5174", bg_color="black", width=300, height=30)
+        self.label_image.grid(row=0, column=0, sticky="nswe", pady=10, padx=10)
+
+        self.label_dataset = customtkinter.CTkLabel(master=self.frame_dataset,
+        text="Result", fg_color="#0c5174", bg_color="black", width=320, height=30)
+        self.label_dataset.grid(row=0, column=0, sticky="nswe", pady=10, padx=10)
+
+        # self.frame_image = customtkinter.CTkFrame(master=self.frame_picture, width=320, border_width=10) # font name and size in px 
+        print(filename)
+        if (filename != ''):
+            image = Image.open(filename).resize((256, 256))
+
+            self.photo = ImageTk.PhotoImage(image)
+
+            self.label_image = customtkinter.CTkLabel(master=self.frame_picture,
+            image=self.photo, width=256, height=256)
+
+            self.label_image.grid(row=1, column=0, sticky="nswe", pady=10, padx=10)
+
+        if (solve):
+            image2 = Image.open("hasil.jpg").resize((256, 256))
+            self.photo2 = ImageTk.PhotoImage(image2)
+
+            self.label_image2 = customtkinter.CTkLabel(master=self.frame_dataset,
+            image=self.photo2)
+
+            self.label_image2.grid(row=1, column=0, sticky="nswe", pady=50, padx=10)
+        # Frame picture akan digunakan untuk menempatkan gambar yang diupload
+
+        # ============ frame_bottom ============
+        # configure grid layout (1x1)
+        
+        
+        # Frame label akan digunakan untuk menampilkan hasil dari face recognition
+
+    def process(self):
+        global execute_time
+        global solve
+        self.start_time = time.time()
+        hasil = answer(dataset, filename)
+        execute_time = time.time() - self.start_time
+        print(execute_time)
+        solve = True
+        cv2.imwrite("hasil.jpg", hasil)
+        print("AKWOAKWOKA")
+        self.destroy()
+        print("CEK APAKAH DESTORY NYA MASALAH")
+        refresh()
+        print("CEK APAKAH REFRESH NYA MASALAH")
+
+    def UploadActionfolder(self):
+        global dataset
+        temp = filedialog.askdirectory()
+        print('Selected: ', temp)
+        if (temp != dataset or temp != ""):
+            dataset = temp
+            print("Dataset changed to: ", dataset)
+            self.update()
+
+    def update(self):
+        self.destroy()
+        refresh()
+
+    def UploadActionImage(self):
         global filename
         temp = filedialog.askopenfilename()
         print('Selected: ', temp)
-        if (temp != filename and temp != ""):
+        if (temp != filename or temp != ""):
             filename = temp
-            self.destroy()
-            refresh()
+            self.update()
 
 
-    def     button_event(self):
+    def button_event(self):
         print("Button pressed")
     def change_appearance_mode(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -114,6 +178,7 @@ class App(customtkinter.CTk):
         self.destroy()
 
 def refresh():
+    print("sok2")
     app = App()
     print("sok")
     app.mainloop()
--- src/tess.py
+++ src/tess.py
@@ -35,9 +35,9 @@ def transpose_matrix(matrix):
 def multiply_matrix(matrix1, matrix2):
     return np.matmul(matrix1, matrix2)
 
-def displayMat(mat):
-    for i in range(len(mat)):
-        print(mat[i])
+# def displayMat(mat):
+#     for i in range(len(mat)):
+#         # print(mat[i])
 
 def createzero(n):
     mat = [[0 for i in range(n)] for j in range(n)]
@@ -120,9 +120,9 @@ def getEigenVal(matrix,listt):
 # m1 = [[2,0,1],[1,2,0],[0,2,4]]
 # m2 = [[1,1,1],[0,1,0],[1,2,2]]
 # data = [[m1,m2]]
-# print(len(data))
-# print(len(data[0]))
-# print(len(data[0][0]))
+# # print(len(data))
+# # print(len(data[0]))
+# # print(len(data[0][0]))
 
 
 # mean = avg_matrix(data)
@@ -132,15 +132,15 @@ def getEigenVal(matrix,listt):
 # hasil_selisih = nilai_tengah(mean,hasil_selisih,data)
 
 # for i in range(len(hasil_selisih)):
-#     print("\n")
+#     # print("\n")
 #     displayMat(hasil_selisih[i])
     
 # temp = hasil_selisih[0]
 # for i in range(len(hasil_selisih) - 1):
 #     temp = merge_matrix(temp, hasil_selisih[i+1])
 
-# print(temp)
-# print("\n")
+# # print(temp)
+# # print("\n")
 
 # cov = np.cov(temp)
 # displayMat(cov)
@@ -153,203 +153,200 @@ def getEigenVal(matrix,listt):
 # x = merge_matrix(m,mat)
 # # x = merge_matrix(mat, matt)
 # # x = merge_matrix(x, mattt)
-# print((x))
+# # print((x))
 
 # path = 'dataset/pins_Zendaya'
-start = time.time()
-newIMG = cv2.imread('avril.jpg', 0)
-newIMG = cv2.resize(newIMG, (256,256), interpolation = cv2.INTER_AREA)
+def answer(datasets,filename):
+    print(type(filename), filename)
+    print(type(datasets), datasets)
+    start = time.time()
+    newIMG = cv2.imread(filename, 0)
+    newIMG = cv2.resize(newIMG, (256,256), interpolation = cv2.INTER_AREA)
 
-count = 0
-dataset = []
-imagee = []
-nama_folder = []
-list_subfolders_with_paths = [f.path for f in os.scandir('dataset') if f.is_dir()]
-for j in (list_subfolders_with_paths):
-    listt = []
-    temp_img = []
-    print(j)
-    for i in os.listdir(j):
-        image = cv2.imread(j+'/'+str(i), 0)
-        img2 = cv2.imread(j+'/'+str(i))
-        resized = cv2.resize(image, (256,256), interpolation = cv2.INTER_AREA)
-        listt.append(resized)
-        temp_img.append(img2)
-    count += 1
-    imagee.append(temp_img)
-    dataset.append(listt)
-    nama_folder.append(j)
-    # if j == "dataset\pins_Katherine Langford":
-    #     break
-    if count == 12:
-        break
-# dataset = np.array(dataset)
-    
-# print(len(dataset[0]))
+    count = 0
+    dataset = []
+    imagee = []
+    nama_folder = []
+    list_subfolders_with_paths = [f.path for f in os.scandir(datasets) if f.is_dir()]
+    for j in (list_subfolders_with_paths):
+        listt = []
+        temp_img = []
+        print(j)
+        for i in os.listdir(j):
+            image = cv2.imread(j+'/'+str(i), 0)
+            img2 = cv2.imread(j+'/'+str(i))
+            resized = cv2.resize(image, (256,256), interpolation = cv2.INTER_AREA)
+            listt.append(resized)
+            temp_img.append(img2)
+        count += 1
+        imagee.append(temp_img)
+        dataset.append(listt)
+        nama_folder.append(j)
+        # if j == "dataset\pins_Katherine Langford":
+        #     break
+        if count == 4:
+            break
+    # dataset = np.array(dataset)
+        
+    # # print(len(dataset[0]))
 
-# print(dataset[0][0])
-#rata-rata
-eigenface = []
-eigen_vector_list = []
-mean_list = []
+    # # print(dataset[0][0])
+    #rata-rata
+    eigenface = []
+    eigen_vector_list = []
+    mean_list = []
 
-for data in range(len(dataset)):
-    print(data + 1)     
-    matrix = avg_matrix(dataset[data])
-    # matrix = matrix.astype(np.uint8)
-    mean_list.append(matrix)
-    # displayMat(matrix)
-    # cv2.imshow('avg image',matrix)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-
-#cari nilai tengah
-    hasil_selisih = []
-    hasil_selisih = nilai_tengah(matrix,hasil_selisih,dataset[data])
-# cv2.imshow("dddd",hasil_selisih[67])
-    # cv2.imshow('selisih',hasil_selisih[5])
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-# for i in range(len(hasil_selisih)):
-#     print(hasil_selisih[i])
-
-    # print("====================================")
-    temp = hasil_selisih[0]
-    # temp = createzero(256)
-    for i in range(1, len(hasil_selisih)):
-        temp = merge_matrix(temp, hasil_selisih[i])
-    # print("==================================")
-    # cv2.imshow('Original image',temp)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    
-
-
-# # # temp = [[1,0,0,0,1,0],[1,1,0,0,0,0],[0,0,1,1,0,1]]
-
-# # #cari kovarian
-##covarian = get_covarian(hasil_selisih)
-# # # print(covarian)
-# # print("==================================")
-# temp = (np.cov(temp))
-    covarian = np.matmul(temp, np.transpose(temp))
-
-# cv2.imshow('Original image',covarian)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# print(len(covarian))
-# print(len(covarian[0]))
-
-
-# print(len(covarian[0]))
-
-    eigen = (getEigenValues(covarian))
-# # print(eigen[0])
-# print("\n")
-# eigenVal =[]
-# eigenVal = getEigenVal(eigen[0],eigenVal)  
-
-    eigen_vector = eigen[1]
-# print(eigen_vector)
-    # print("============================================================")
-    #eigen_vector = np.linalg.eig(covarian)[1]
-    # print(eigen_vector)
-
-
-    # for i in range(len(eigen_vector)):
-    #     norm = np.linalg.norm(eigen_vector[:,i])
-    #     temp = (1/norm)*eigen_vector[:,i]
-    #     listt.append(temp)
-    listt =[]
-    for i in range(len(hasil_selisih)):
-        # temp = []
-        temp3 = multiply_matrix(eigen_vector, hasil_selisih[i])
-        # cv2.imshow('Original image'+str(i),temp3)
+    for data in range(len(dataset)):
+        print(data + 1)     
+        matrix = avg_matrix(dataset[data])
+        # matrix = matrix.astype(np.uint8)
+        mean_list.append(matrix)
+        # displayMat(matrix)
+        # cv2.imshow('avg image',matrix)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
-        listt.append(temp3)
+
+
+    #cari nilai tengah
+        hasil_selisih = []
+        hasil_selisih = nilai_tengah(matrix,hasil_selisih,dataset[data])
+    # cv2.imshow("dddd",hasil_selisih[67])
+        # cv2.imshow('selisih',hasil_selisih[5])
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+    # for i in range(len(hasil_selisih)):
+    #     # print(hasil_selisih[i])
+
+        # # print("====================================")
+        temp = hasil_selisih[0]
+        # temp = createzero(256)
+        for i in range(1, len(hasil_selisih)):
+            temp = merge_matrix(temp, hasil_selisih[i])
+        # # print("==================================")
+        # cv2.imshow('Original image',temp)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
         
-    eigenface.append(listt)
-    eigen_vector_list.append(eigen_vector)
-
-# print((hasil_selisih[0]))
 
 
+    # # # temp = [[1,0,0,0,1,0],[1,1,0,0,0,0],[0,0,1,1,0,1]]
+
+    # # #cari kovarian
+    ##covarian = get_covarian(hasil_selisih)
+    # # # # print(covarian)
+    # # # print("==================================")
+    # temp = (np.cov(temp))
+        covarian = np.matmul(temp, np.transpose(temp))
+
+    # cv2.imshow('Original image',covarian)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # # print(len(covarian))
+    # # print(len(covarian[0]))
 
 
-# displayMat(listt)
-# print(listt[:,0])
-# print(len(listt))
-# print(len(listt[0]))
-# print(len(listt[0][0]))
-# for i in range(5):
-#     cv2.imshow('Original image'+str(i),listt[i])
+    # # print(len(covarian[0]))
 
-# cv2.waitKey(0)    
-# cv2.destroyAllWindows()
+        eigen = (getEigenValues(covarian))
+    # # # print(eigen[0])
+    # # print("\n")
+    # eigenVal =[]
+    # eigenVal = getEigenVal(eigen[0],eigenVal)  
 
-
-# newIMG = cv2.imread('gal-gadot.jpeg', 0)
-# newIMG = cv2.resize(newIMG, (256,256), interpolation = cv2.INTER_AREA)
-
+        eigen_vector = eigen[1]
+    # # print(eigen_vector)
+        # # print("============================================================")
+        #eigen_vector = np.linalg.eig(covarian)[1]
+        # # print(eigen_vector)
 
 
+        # for i in range(len(eigen_vector)):
+        #     norm = np.linalg.norm(eigen_vector[:,i])
+        #     temp = (1/norm)*eigen_vector[:,i]
+        #     listt.append(temp)
+        listt =[]
+        for i in range(len(hasil_selisih)):
+            # temp = []
+            temp3 = multiply_matrix(eigen_vector, hasil_selisih[i])
+            # cv2.imshow('Original image'+str(i),temp3)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            listt.append(temp3)
+            
+        eigenface.append(listt)
+        eigen_vector_list.append(eigen_vector)
 
-hasil_euclidian = []
-list_min = []
-for vector in range(len(eigen_vector_list)):
-    print(vector+1)
-    testIMG = subtract_matrix(newIMG, mean_list[vector])
-    newtst = multiply_matrix(eigen_vector_list[vector], testIMG)
-    min = 0
+    # # print((hasil_selisih[0]))
+
+
+
+
+    # displayMat(listt)
+    # # print(listt[:,0])
+    # # print(len(listt))
+    # # print(len(listt[0]))
+    # # print(len(listt[0][0]))
+    # for i in range(5):
+    #     cv2.imshow('Original image'+str(i),listt[i])
+
+    # cv2.waitKey(0)    
+    # cv2.destroyAllWindows()
+
+
+    # newIMG = cv2.imread('gal-gadot.jpeg', 0)
+    # newIMG = cv2.resize(newIMG, (256,256), interpolation = cv2.INTER_AREA)
+
+
+
+
+    hasil_euclidian = []
+    list_min = []
+    for vector in range(len(eigen_vector_list)):
+        print(vector+1)
+        testIMG = subtract_matrix(newIMG, mean_list[vector])
+        newtst = multiply_matrix(eigen_vector_list[vector], testIMG)
+        min = 0
+        idx_min = 0
+        # for j in range(len(eigenface)):
+        euiclidian = []
+        # temp_min = []
+        for i in range(len(eigenface[vector])):
+            temp = subtract_matrix(newtst, eigenface[vector][i])
+            temp = getNormMat(temp)
+            euiclidian.append(temp)
+            # temp_min.append(temp)
+            if i == 0:
+                min = temp
+                idx_min = 0
+            elif temp <= min:
+                min = temp
+                idx_min = i
+            # if(temp<=100):
+            #     # print("ada")
+            #     # print(i)
+            #     break
+            # minnnnnnnn = min(temp_min)
+            # # print(temp_min)
+            # # print("====================================")
+        list_min.append((min, idx_min))
+        hasil_euclidian.append(euiclidian)
+    
+    # hasil_euclidian = np.array(hasil_euclidian)   
+    # minn = np.unravel_index(np.argmin(hasil_euclidian, axis=None), hasil_euclidian.shape)
+    # # print(list_min)
     idx_min = 0
-    # for j in range(len(eigenface)):
-    euiclidian = []
-    # temp_min = []
-    for i in range(len(eigenface[vector])):
-        temp = subtract_matrix(newtst, eigenface[vector][i])
-        temp = getNormMat(temp)
-        euiclidian.append(temp)
-        # temp_min.append(temp)
-        if i == 0:
-            min = temp
-            idx_min = 0
-        elif temp <= min:
-            min = temp
+    final_min = list_min[0]
+    for i in range(len(list_min)):
+        if list_min[i][0] <= final_min[0]:
+            final_min = list_min[i]
             idx_min = i
-        # if(temp<=100):
-        #     print("ada")
-        #     print(i)
-        #     break
-        # minnnnnnnn = min(temp_min)
-        # print(temp_min)
-        # print("====================================")
-    list_min.append((min, idx_min))
-    hasil_euclidian.append(euiclidian)
- 
-# hasil_euclidian = np.array(hasil_euclidian)   
-# minn = np.unravel_index(np.argmin(hasil_euclidian, axis=None), hasil_euclidian.shape)
-# print(list_min)
-idx_min = 0
-final_min = list_min[0]
-for i in range(len(list_min)):
-    if list_min[i][0] <= final_min[0]:
-        final_min = list_min[i]
-        idx_min = i
 
-print("==============DISTANCE================")
+    # print("==============DISTANCE================")
 
-for i in range(len(list_min)):
-    print(nama_folder[i], "=", list_min[i][0])
-       
-# index = hasil_euclidian.index(minn)
-img = imagee[idx_min][final_min[1]]
+        # print(nama_folder[i], "=", list_min[i][0])
+        
+    # index = hasil_euclidian.index(minn)
+    img = imagee[idx_min][final_min[1]]
 
-
-print("Times =",time.time()-start,"seconds")
-
-cv2.imshow('Original image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return img
